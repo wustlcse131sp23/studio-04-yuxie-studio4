8535381d13352681caaf85bc1a15e1b37e53b354
xiejuan <xiejuan@xiejuandembp.wusm-wifi.wucon.wustl.edu>
2023-02-23 12:13:29 -0600

    Studio

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,18 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(Color.red);
+		StdDraw.filledRectangle(.5, .9, 0.3, 0.1);
+		
+		StdDraw.setPenColor(Color.white);
+		StdDraw.filledRectangle(.5, .7, 0.3, 0.1);
+		
+		StdDraw.setPenColor(Color.blue);
+		StdDraw.filledRectangle(.5, .5, 0.3, 0.1);
+		
+	
+	
+		StdDraw.show();
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,7 @@
 package studio4;
 
+
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +21,43 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+
+		StdDraw.clear();
 		
+		String name = in.next();
+
+		int r = 0;
+		int g = 0;
+		int b = 0 ;
+		
+		r = in.nextInt();
+		g = in.nextInt();
+		b = in.nextInt();
+		
+		
+		
+		Color black = new Color(r, g, b);
+		StdDraw.setPenColor(black);
+		
+		boolean m = true;
+		m = in.nextBoolean();
+	
+		double s1 = 0.0, s2=0.0, s3=0.0, s4=0.0;
+		s1 = in.nextDouble();
+		s2 = in.nextDouble();
+		s3 = in.nextDouble();
+		s4 = in.nextDouble();
+	
+
+		if (m = true) {
+			StdDraw.filledRectangle(s1, s2, s3, s4);
+		}
+		else {
+			StdDraw.rectangle(s1, s2, s3, s4);
+		}
+		
+		StdDraw.show();
+		
+
 	}
 }
